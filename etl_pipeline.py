699b55f1c1742b6b7c280c77ddf48889f0a3943d
devgupta24 <96465425+devgupta24@users.noreply.github.com>
2026-05-24 08:51:16 +0000

    Added Prometheus metrics

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,5 +1,4 @@
 import pandas as pd
 df = pd.DataFrame([["Alice",48,"Engineer"],["Bob",28,"Designer"]],
                   columns=["Name","Age","Role"])
-                  
 print(df.to_string(index=False))
